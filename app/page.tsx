6b02f1eadd84e586629822501c87b1b379e0fe22
mirjananistor-source <mirjananistor@gmail.com>
2026-04-17 19:32:16 +0200

    restore basic home layout (Navbar + Hero)

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-
 export const dynamic = "force-dynamic"
 
-export default function Home() {
-  return (
-    <main style={{ padding: 24, fontFamily: "system-ui" }}>
-      <h1>NiStor website</h1>
-      <p>Deployment OK. (Privremena stranica)</p>
-    </main>
-  )
-}
+import HomeClient from "@/components/home-client"
 
+export default function Home() {
+  return <HomeClient />
+}
+``
 
